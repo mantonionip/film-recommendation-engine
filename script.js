b6cb568b63521063eda0fb810e2e8f9c4f3875fa
Mehdi Pilehvarian <mehdi.pilehvarian@gmail.com>
2019-11-21 19:18:47 -0500

    second commit

--- script.js
+++ script.js
@@ -1,48 +1,71 @@
-// Create app namespace to hold all methods
+const movieApp = {};
 
-// Collect user input
-// app.collectInfo = function () {
-
-// }
-
-const app = {};
-
-app.apiKey = "ddeff0fdf2d0c94ee533861e5e5bd368";
+movieApp.apiKey = "ddeff0fdf2d0c94ee533861e5e5bd368";
 
 // these will be our input variables
-app.genreID = 28; //action movie genre
-app.releaseYear = 2019; //year 
-app.voterAverage = 8;
+movieApp.genreID = 0; //action movie genre
+movieApp.releaseYear = 0; //year 
+movieApp.voterAverage = 8;
 
 // Make AJAX request with user inputted data
-app.getInfo = function () {
-    $.ajax({
+movieApp.getInfo = function () {
+    movieApp.yearOfMaking();
+    movieApp.movieGenre();
+    return $.ajax({
         url: "https://api.themoviedb.org/3/discover/movie",
         method: "GET",
         dataType: "json",
         data: {
-            api_key: app.apiKey,
-            with_genres: app.genreID,
-            "original_language": "en",
-            primary_release_year: app.releaseYear,
-            // "vote_average.gte": app.voterAverage,
+            api_key: movieApp.apiKey,
+            with_genres: movieApp.genreID,
+            primary_release_year: movieApp.releaseYear,
+            "vote_average.gte": 7,
+            // "vote_count.gte": 500
+            sort_by: "vote_count.desc",
         }
     }).then((result) => {
-        console.log(result);
+        movieApp.newResult = result.results.slice(0, 3);
+        // console.log(newResult);
+        // let movieHtml = `<h2>${newResult[0].title}</h2> <p>${newResult[0].overview}</p> <img src="https://image.tmdb.org/t/p/w500${newResult[0].poster_path}" alt="${newResult[0].title} poster">`
+        // $(".resultPageSection").append(movieHtml)
+        $(".resultPageSection").empty()
+        movieApp.newResult.forEach((movie, index) => {
+            let movieHtml = `<button value="${index}"><img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} poster"></button>`;
+            // movieHtml = `${movie.poster_path}`
+            $(".resultPageSection").append(movieHtml)
+            
+            $(".resultPageSection button").css({
+                "width": "calc(100% / 3)",
+            })
+        })
     })
 }
 
+// Function to display the selected film's info
+
+
+
+// Function to select the year by the user
+movieApp.yearOfMaking = function() {
+        movieApp.releaseYear = $('#year').val();
+}
+
+// Function to select the genre by the user on-click event handler
+movieApp.movieGenre = function() {
+        movieApp.genreID = $("input[type=radio]:checked").val();
+}
+
 $(function () {
-    // app.init();
-    app.getInfo();
+    $("input[type=submit]").on("click", function(e){
+        e.preventDefault();
+        movieApp.getInfo();
+    });
+    $('.resultPageSection').on('click', "button", function() {
+        const displaySwitch = $(this).val();
+        // console.log(movieApp.newResult[displaySwitch]);
+
+        let movieHtml = `<h2>${movieApp.newResult[displaySwitch].title}</h2> <p>${movieApp.newResult[displaySwitch].overview}</p>`
+        $(".finalResult").empty()
+        $(".finalResult").append(movieHtml);
+    })
 });
-
-// Display data on the page
-// app.displayInfo = function () {
-
-// }
-
-// // Start app
-// app.init = function () {
-
-// }
